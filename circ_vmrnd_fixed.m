15e0bcff4a760bc08a37d7f97af9d6360c57a47f
jCalderTravis <38797399+jCalderTravis@users.noreply.github.com>
2021-04-26 12:29:08 +0200

    Updating after changes to circ_vmrnd

--- circ_vmrnd_fixed.m
+++ circ_vmrnd_fixed.m
@@ -1,25 +1,18 @@
 function samples = circ_vmrnd_fixed(mu, kappa, shape)
-% Returns values on the interval [0 2*pi] rather than [-pi pi], if necessary.
-% Reshape the output, if necessary.
+% For kappa_s ~ 0, circ_vmrnd such does not reshape the output. Do the
+% reshaping.
+
+assert(isequal(size(mu), [1, 1]))
+assert(isequal(size(kappa), [1, 1]))
+% Not sure what the reshaping will do if these are not scalars
 
 samples = circ_vmrnd(mu, kappa, shape);
 
-
 if kappa < 1e-6
-    
-    samples = samples - pi;
-    
-    
-    samples = reshape(samples, shape);
-    
-    
+    samples = reshape(samples, shape);    
 end
 
-
-% Defensive programming
-if any(any(samples > pi)) || any(any(samples < -pi))
-    
+if any(samples(:) > pi) || any(samples(:) < -pi)    
     error('Von Misses function is returning unexpected values')
-    
-    
 end
+
